668c3c48166818ab14ff5b908c3f5044f7097b33
sharjeel <muhammadsharjeel911@gmail.com>
2021-12-08 03:12:53 +0500

    new update 1.1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "SharjeelVPC" {
-  cidr_block           = SharjeelVPC
+  cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
